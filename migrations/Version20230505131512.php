2cf70d7eebf0775b9facae8c417806a081baa553
Miguel V.C <wmigue@gmail.com>
2023-05-05 10:22:26 -0300

    5-4-2023

---
+++ migrations/Version20230505131512.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230505131512 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE imagenes (id INT AUTO_INCREMENT NOT NULL, url VARCHAR(500) NOT NULL, texto VARCHAR(30) NOT NULL, alto INT NOT NULL, ancho INT NOT NULL, colores TEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(20) NOT NULL, mail VARCHAR(50) NOT NULL, password VARCHAR(20) NOT NULL, imagen VARCHAR(1000) DEFAULT \'NULL\', colores TEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE imagenes');
+        $this->addSql('DROP TABLE users');
+    }
+}
